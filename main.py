4916ff9d190a40e5ba62b0ca54b77d0cd52e7a71
jvv1993 <43385475+jvv1993@users.noreply.github.com>
2023-06-02 21:22:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from utils.metric import weather_metric, e2e_metric
 sys.path.insert(0, './nn')
 #from dualVAE import DualVAE
 #from dualVAE_classify import DualVAE_classify
-from DualSentiment import DualSentiment
+import DualSentiment
 
 def	update_once_unsup(dataset, model, log, unsup_type):
 	'''
